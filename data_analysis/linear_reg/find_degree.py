eb5f17e9974d64045be0c67758f06d067bf4226a
vivaldogp <VivaldoGP27@hotmail.com>
2024-03-29 01:18:38 -0600

    feat: all stats included

---
+++ data_analysis/linear_reg/find_degree.py
@@ -0,0 +1,95 @@
+import os
+
+from json import loads, dumps
+import pandas as pd
+import statsmodels.api as sm
+
+from plot_utils.charts import poly_degree_aic
+
+import matplotlib.pyplot as plt
+
+from stats_utils.regression_models import linear_reg_model
+
+parcela = int(input('Ingrese el id de la parcela: '))
+variable = str(input('Ingrese la variable a analizar: '))
+
+data_test = rf"C:\Users\Isai\Documents\Tesis\code\datos\parcelas\ready_to_analyze\parcela_{parcela}.csv"
+
+coef_folder = rf"C:\Users\Isai\Documents\Tesis\code\data_analysis\linear_reg\coeficientes"
+variable_folder_coef = os.path.join(coef_folder, variable)
+if not os.path.exists(variable_folder_coef):
+    os.makedirs(variable_folder_coef)
+
+aic_img_folder = rf"C:\Users\Isai\Documents\Tesis\code\imagenes_tesis\aic_vs_degrees"
+variable_folder_aic = os.path.join(aic_img_folder, variable)
+if not os.path.exists(variable_folder_aic):
+    os.makedirs(variable_folder_aic)
+
+model_predicts_folder = fr"C:\Users\Isai\Documents\Tesis\code\data_analysis\datos\model_predicts"
+variable_folder_predicts = os.path.join(model_predicts_folder, variable)
+if not os.path.exists(variable_folder_predicts):
+    os.makedirs(variable_folder_predicts)
+
+polys_img_folder = fr"C:\Users\Isai\Documents\Tesis\code\imagenes_tesis\polys_vs_data"
+variable_folder_polys = os.path.join(polys_img_folder, variable)
+if not os.path.exists(variable_folder_polys):
+    os.makedirs(variable_folder_polys)
+
+parcela_id = data_test.split('_')[-1].split('.')[0]
+df = pd.read_csv(data_test)
+
+metadata, modelos = linear_reg_model(df, ['Dia', variable])
+print(metadata)
+meta_json = loads(metadata.to_json(orient='records'))
+meta_to_export = dumps(meta_json, indent=4)
+
+with open(os.path.join(variable_folder_coef, rf"parcela_{parcela}.json"), 'w') as f:
+    f.write(meta_to_export)
+print(modelos.keys())
+
+poly_degree_aic(metadata, 'degree', ['aic', 'rsquared'], 'AIC vs Grado del polinomio',
+                'Grado del polinomio', ['AIC', '$R^2$'], subtitle=f'Parcela {parcela_id}',
+                export=True,
+                export_path=os.path.join(variable_folder_aic, rf"parcela_{parcela}.jpg"))
+
+x = df['Dia'].values.reshape(-1, 1)
+y = df[variable].values
+
+# Crear subfiguras
+fig, ax = plt.subplots(3, 3, figsize=(18, 12))
+fig.suptitle(f'Parcela {parcela_id}', fontsize=16)
+ax = ax.flatten()
+
+# Asegurarse de que haya al menos 9 modelos en 'modelos'
+for degree in range(1, 10):
+    if degree not in modelos:
+        continue
+
+    model_info = modelos[degree]
+    poly_degree = model_info[1]
+    model = model_info[0]
+
+    x_poly = poly_degree.fit_transform(x)
+    y_pred = model.predict(sm.add_constant(x_poly))
+    df[f'y_pred_{degree}'] = y_pred
+
+    ax[degree - 1].scatter(df['Dia'], df[variable], color='lightcoral', label='Datos reales', marker='D', s=3)
+    ax[degree - 1].plot(x, y_pred, label=f'Modelo (grado {degree})', linestyle='--', linewidth=0.7, color='royalblue')
+    ax[degree - 1].set_title(f'Grado {degree}')
+    ax[degree - 1].set_xlabel('Día')
+    ax[degree - 1].set_ylabel(variable.split('_')[0].upper())
+    ax[degree - 1].legend(loc='lower right')
+
+    box_text = f'$R^2$:{model.rsquared:.3f}\nAIC: {model.aic:.3f}\nMSE: {model.mse_model:.3f}'
+    ax[degree - 1].text(0.9, 0.9, box_text, transform=ax[degree - 1].transAxes,
+                        bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5'), fontsize=8)
+
+print(df)
+df.to_csv(os.path.join(variable_folder_predicts, rf"parcela_{parcela}.csv"), index=False)
+plt.subplots_adjust(hspace=1.2, wspace=1)
+
+plt.tight_layout()
+# plt.suptitle(f'Parcela {parcela_id}', fontsize=10)
+plt.savefig(os.path.join(variable_folder_polys, rf"parcela_{parcela}.jpg"), dpi=600,
+            bbox_inches='tight')
+plt.show()
