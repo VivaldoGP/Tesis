f200ba265cdb6368f1fcd743dfa442cb1dd6fabb
vivaldogp <VivaldoGP27@hotmail.com>
2024-04-01 23:49:24 -0600

    feat: arima model

---
+++ stats_utils/arima.py
@@ -0,0 +1,11 @@
+from statsmodels.tsa.arima.model import ARIMA
+
+from pandas import DataFrame
+
+
+def arima_model(data: DataFrame, order: tuple):
+
+    model = ARIMA(data, order=order)
+    model_fit = model.fit()
+
+    return model, model_fit
