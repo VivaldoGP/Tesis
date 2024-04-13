d476d9eeac20b17f584bf84b4ff9166cf11edfa6
vivaldogp <VivaldoGP27@hotmail.com>
2024-04-13 00:35:05 -0600

    feat: fix coefs

--- data_analysis/all_vars/join_vars.py
+++ data_analysis/all_vars/join_vars.py
@@ -5,17 +5,37 @@ import json
 
 zafra = int(input("Zafra: "))
 
-coefs_dir = rf"C:\Users\Isai\Documents\Tesis\code\data_analysis\linear_reg\coeficientes\zafra{zafra}\ndvi_mean"
+ndvi_coefs_dir = rf"C:\Users\Isai\Documents\Tesis\code\data_analysis\linear_reg\coeficientes\zafra{zafra}\ndvi_mean"
 ndvi_dir = rf"C:\Users\Isai\Documents\Tesis\code\data_analysis\datos\model_predicts\zafra{zafra}\ndvi_mean"
 
-for parcela in os.listdir(coefs_dir):
+ndmi_coefs_dir = rf"C:\Users\Isai\Documents\Tesis\code\data_analysis\linear_reg\coeficientes\zafra{zafra}\ndmi_mean"
+ndmi_dir = rf"C:\Users\Isai\Documents\Tesis\code\data_analysis\datos\model_predicts\zafra{zafra}\ndmi_mean"
+
+msi_coefs_dir = rf"C:\Users\Isai\Documents\Tesis\code\data_analysis\linear_reg\coeficientes\zafra{zafra}\msi_mean"
+msi_dir = rf"C:\Users\Isai\Documents\Tesis\code\data_analysis\datos\model_predicts\zafra{zafra}\msi_mean"
+
+for parcela in os.listdir(ndvi_coefs_dir):
     parcela_id = int(parcela.split('_')[1].split('.')[0])
-    with open(os.path.join(coefs_dir, parcela), 'r') as file:
+    with open(os.path.join(ndvi_coefs_dir, parcela), 'r') as file:
         coefs = json.load(file)
         print(f"Parcela: {parcela_id}")
         params = coefs[1]['params']
-        c, b, a = params[0], params[1], params[2]
-        print(f"Coeficientes: {a}, {b}, {c}")
+        c_ndvi, b_ndvi, a_ndvi = params[0], params[1], params[2]
+        print(f"Coeficientes: {a_ndvi}, {b_ndvi}, {c_ndvi}")
+
+    with open(os.path.join(ndmi_coefs_dir, parcela), 'r') as file:
+        coefs = json.load(file)
+        print(f"Parcela: {parcela_id}")
+        params = coefs[1]['params']
+        c_ndmi, b_ndmi, a_ndmi = params[0], params[1], params[2]
+        print(f"Coeficientes: {a_ndmi}, {b_ndmi}, {c_ndmi}")
+
+    with open(os.path.join(msi_coefs_dir, parcela), 'r') as file:
+        coefs = json.load(file)
+        print(f"Parcela: {parcela_id}")
+        params = coefs[1]['params']
+        c_msi, b_msi, a_msi = params[0], params[1], params[2]
+        print(f"Coeficientes: {a_msi}, {b_msi}, {c_msi}")
 
     for file in os.listdir(ndvi_dir):
         if int(file.split('_')[1].split('.')[0]) == parcela_id:
@@ -26,8 +46,12 @@ for parcela in os.listdir(coefs_dir):
             dias = [start_date + pd.Timedelta(days=i) for i in range((end_date - start_date).days + 1)]
             dias_serie = pd.Series(dias)
             df = pd.DataFrame({'Fecha': dias_serie, 'dias': np.linspace(1, len(dias), len(dias))})
-            df['ndvi'] = a * df['dias'] ** 2 + b * df['dias'] + c
-            print(df.head(5))
+            df['ndvi'] = a_ndvi * df['dias'] ** 2 + b_ndvi * df['dias'] + c_ndvi
+            print(f"parcela: {parcela_id}, coefs: {a_ndvi}, {b_ndvi}, {c_ndvi}")
+            df['ndmi'] = a_ndmi * df['dias'] ** 2 + b_ndmi * df['dias'] + c_ndmi
+            print(f"parcela: {parcela_id}, coefs: {a_ndmi}, {b_ndmi}, {c_ndmi}")
+            df['msi'] = a_msi * df['dias'] ** 2 + b_msi * df['dias'] + c_msi
+            print(f"parcela: {parcela_id}, coefs: {a_msi}, {b_msi}, {c_msi}")
 
             for temp_file in os.listdir(r"C:\Users\Isai\Documents\Tesis\code\data_analysis\datos\fourier\temperature"):
                 if int(temp_file.split('_')[1].split('.')[0]) == parcela_id:
