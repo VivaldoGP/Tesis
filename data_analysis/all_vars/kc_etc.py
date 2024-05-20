39af87a8c90352833c813a60b32894255e4a5bff
vivaldogp <VivaldoGP27@hotmail.com>
2024-05-20 00:41:54 -0600

    feat: select zafra

--- data_analysis/all_vars/kc_etc.py
+++ data_analysis/all_vars/kc_etc.py
@@ -3,10 +3,11 @@ from pathlib import PurePath
 
 import pandas as pd
 
-
-for file in os.listdir(r'C:\Users\Isai\Documents\Tesis\code\data_analysis\all_vars\zafra2022'):
-    data = pd.read_csv(PurePath(r'C:\Users\Isai\Documents\Tesis\code\data_analysis\all_vars\zafra2022', file))
-    data['kc'] = 1.15 * data['ndvi'] + 0.17
+zafra = 2022
+for file in os.listdir(rf'C:\Users\Isai\Documents\Tesis\code\data_analysis\all_vars\zafra{zafra}'):
+    data = pd.read_csv(PurePath(fr'C:\Users\Isai\Documents\Tesis\code\data_analysis\all_vars\zafra{zafra}', file))
+    data['kc'] = (1.15 * data['ndvi']) + 0.17
     data['etc'] = data['kc'] * data['et']
     data['etc_acum'] = data['etc'].cumsum()
-    data.to_csv(PurePath(r'C:\Users\Isai\Documents\Tesis\code\data_analysis\all_vars\zafra2022', file), index=False)
+    data['et_acum'] = data['et'].cumsum()
+    data.to_csv(PurePath(fr'C:\Users\Isai\Documents\Tesis\code\data_analysis\all_vars\zafra{zafra}', file), index=False)
