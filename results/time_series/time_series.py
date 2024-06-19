084795eff9094f7473d1b4b3f3f5de2733de3a7d
VivaldoGP <VivaldoGP27@hotmail.com>
2024-06-19 16:45:23 -0600

    feat: fix paths & line color

--- results/time_series/time_series.py
+++ results/time_series/time_series.py
@@ -8,8 +8,8 @@ option = int(input('1. Reals\n2. Con modelo\n'))
 
 focus_vars = ['ndvi', 'gndvi', 'msi', 'ndmi', 'cire', 'ndre1']
 
-reales = rf"C:\Users\Isai\Documents\Tesis\code\datos\parcelas\ready_to_analyze\zafra{zafra}"
-modelados = rf"C:\Users\Isai\Documents\Tesis\code\data_analysis\all_vars\zafra{zafra}"
+reales = rf"../../datos/parcelas/ready_to_analyze/zafra{zafra}"
+modelados = rf"../../data_analysis/all_vars/zafra{zafra}"
 
 modelados_files = [(pd.read_csv(PurePath(modelados, file), parse_dates=True), int(file.split('.')[0].split('_')[1])
                     ) for file in os.listdir(modelados) if file.endswith(".csv")]
@@ -24,25 +24,25 @@ for i in modelados_files:
                     if option == 2:
                         fig, ax = plt.subplots(figsize=(10, 5))
                         ax.plot(pd.to_datetime(i[0]['Fecha']), i[0][var], label='Modelado', color=(150/255, 0, 24/255))
-                        ax.plot(pd.to_datetime(j[0]['Fecha']), j[0][f"{var}_mean"], label='Real', color=(105/255, 255/255, 231/255))
+                        ax.plot(pd.to_datetime(j[0]['Fecha']), j[0][f"{var}_mean"], label='Real', color='royalblue')
                         ax.set_title(f'Parcela {i[1]} - Índice {var.upper()}')
                         ax.set_ylabel(f'{var.upper()}')
                         ax.set_xlabel('Fecha')
                         ax.legend()
                         plt.tight_layout()
-                        export_path = PurePath(rf"C:\Users\Isai\Documents\Tesis\code\tesis_img\series_tiempo_real_modelo\zafra{zafra}\{var}")
+                        export_path = PurePath(rf"../../tesis_img/series_tiempo_real_modelo/zafra{zafra}/{var}")
                         if not os.path.exists(export_path):
                             os.makedirs(export_path)
-                        plt.savefig(PurePath(export_path, f"parcela_{i[1]}_{var}.png"), dpi=100)
+                        plt.savefig(PurePath(export_path, f"parcela_{i[1]}_{var}.pdf"), dpi=100)
                     elif option == 1:
                         fig, ax = plt.subplots(figsize=(10, 5))
-                        ax.plot(pd.to_datetime(j[0]['Fecha']), j[0][f"{var}_mean"], label='Real', color=(150/255, 0, 24/255))
+                        ax.plot(pd.to_datetime(j[0]['Fecha']), j[0][f"{var}_mean"], label='Real', color='royalblue')
                         ax.set_title(f'Parcela {i[1]} - Índice {var.upper()}')
                         ax.set_ylabel(f'{var.upper()}')
                         ax.set_xlabel('Fecha')
                         ax.legend()
                         plt.tight_layout()
-                        export_path = PurePath(rf"C:\Users\Isai\Documents\Tesis\code\tesis_img\series_tiempo_real\zafra{zafra}\{var}")
+                        export_path = PurePath(rf"../../tesis_img/series_tiempo_real/zafra{zafra}/{var}")
                         if not os.path.exists(export_path):
                             os.makedirs(export_path)
-                        plt.savefig(PurePath(export_path, f"parcela_{i[1]}_{var}.png"), dpi=100)
+                        plt.savefig(PurePath(export_path, f"parcela_{i[1]}_{var}.pdf"), dpi=100)
