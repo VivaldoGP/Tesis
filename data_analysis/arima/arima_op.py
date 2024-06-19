125470f8bc78e42714bddfce7f25670f4eb3e8dd
VivaldoGP <VivaldoGP27@hotmail.com>
2024-06-18 23:38:52 -0600

    feat: arima model

--- data_analysis/arima/arima_op.py
+++ data_analysis/arima/arima_op.py
@@ -5,12 +5,12 @@ from stats_utils.arima import arima_model
 from statsmodels.tools.eval_measures import rmse
 from sklearn.metrics import r2_score
 
-dirs_name = str(input('Ingrese el nombre de la carpeta donde se encuentran los results: '))
+dirs_name = str(input('Ingrese el nombre de la carpeta donde se encuentran los datos: '))
 var_name = str(input('Ingrese el nombre de la variable a analizar: '))
 
-data_dir = rf"C:\Users\Isai\Documents\Tesis\code\datos\agroclimate\{dirs_name}"
-dest_dir = fr"C:\Users\Isai\Documents\Tesis\code\data_analysis\results\arima\{dirs_name}"
-model_metadata = fr"C:\Users\Isai\Documents\Tesis\code\data_analysis\arima\model_metadata\{dirs_name}"
+data_dir = rf"../../datos/agroclimate/{dirs_name}"
+dest_dir = fr"../datos\arima/{dirs_name}"
+model_metadata = fr"model_metadata/{dirs_name}"
 
 if not os.path.exists(dest_dir):
     os.makedirs(dest_dir)
